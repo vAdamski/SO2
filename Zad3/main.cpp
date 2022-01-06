065d45129f76ad1f948aec3acf5253669c9972fd
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2022-01-06 15:59:23 +0100

    Update Zad3

--- Zad3/main.cpp
+++ Zad3/main.cpp
@@ -6,60 +6,60 @@ W przypadku wywołania z liczbą argumentów większą niż 1, tworzy taką licz
 każdy z procesów wyświetla na ekranie jeden argument.
 *Gdy pierwszym argumentem jest "c", wykorzystana zostanie funkcja *clone(…)*, jeżeli jest to inny znak, używany jest
 *fork()*.
- 
+
 Program 3A
 Zachowanie programu jest takie jak w wersji IX/POSIX, z pominięciem opcji wywołanie "c", ale został napisany z
 wykorzystaniem wyłącznie WinAPI (windows.h*).
 Wskazówka: CreateProcess(…)
 */
- 
+
 #include <windows.h>
 #include <cwchar>
 #include <iostream>
- 
+#include <string>
+#include <cstring>
+
 using namespace std;
- 
+
 void zeroMemory(PROCESS_INFORMATION* pi, STARTUPINFO* si)
 {
     ZeroMemory(pi, sizeof(*pi));
     ZeroMemory(si, sizeof(*si));
 }
- 
+
 int main(int argc, char** argv) {
     PROCESS_INFORMATION pi;
     STARTUPINFO si;
-    CreateMutex(NULL, false, (LPCSTR)"myMutex");
- 
- 
- 
- 
-    int err = GetLastError();
-    bool isMainProcess = false;
- 
-    if (!err)
-    {
-        isMainProcess = true;
-    }
- 
+
     zeroMemory(&pi, &si);
- 
+
     si.cb = sizeof(si);
- 
+
     if (argc == 1)
     {
-        if (isMainProcess)
-        {
-            cout << "Brak parametrów" << endl;
-        }
- 
-        return 1;
+        cout << "Brak argumentów" << endl;
+        return 0;
     }
- 
- 
-    for (int i = 1; i < argc ; i++)
+
+    if(argc == 2)
     {
-        int newProcess = CreateProcess(argv[0],
-                                       argv[i],
+        cout << "Argument = " << argv[1] << " prorcess id = " << GetCurrentProcessId() << endl;
+        return 0;
+    }
+
+    for (int i = 1; i < argc ; i++)
+        {
+        string parameter = argv[0];
+        parameter.append(" ");
+        parameter.append(argv[i]);
+
+        int n = parameter.length();
+        char char_array[n + 1];
+        strcpy(char_array, parameter.c_str());
+
+
+        int newProcess = CreateProcess(NULL,
+                                       char_array,
                                        NULL,
                                        NULL,
                                        NULL,
@@ -68,18 +68,16 @@ int main(int argc, char** argv) {
                                        NULL,
                                        &si,
                                        &pi);
- 
+
         if (!newProcess)
         {
             return 1;
         }
-        cout << "Process: "<<argv[i] << endl;
- 
- 
+
         WaitForSingleObject(pi.hProcess, INFINITE);
         CloseHandle(pi.hProcess);
         CloseHandle(pi.hThread);
     }
- 
+
     return 0;
 }
